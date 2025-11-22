ba401c7b36d5aa3ccaa37b7e67778e6e1dc91062
NunSaraam <yjc22950037@gmail.com>
2025-11-22 18:26:39 +0900

    [Feat] 플레이어 무빙까지

--- Paint/Assets/Script/Player/PlayerController.cs
+++ Paint/Assets/Script/Player/PlayerController.cs
@@ -7,5 +7,61 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 3f;
 
+    private bool isGrounded;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float gorundCheckRadius = .2f;
+    public bool facingRight = true;
+    private bool isDead = false;
 
+    public Animator animator;
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+        if (animator == null) animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, gorundCheckRadius, groundLayer);
+        Jump();
+    }
+
+    void PlayerMovement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
 }
